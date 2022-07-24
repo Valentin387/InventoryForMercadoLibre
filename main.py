dee9423515ff6cda537bd05e8d83275ba681835c
Valentin387 <valentin.valencia@utp.edu.co>
2022-07-24 17:37:32 -0500

    now it reads the .xls file correctly

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
+import xlrd
 
 root=Tk()
 root.title("Valentin, MercadoLibre Inventory")
-root.geometry("500x300")
+root.geometry("1000x500")
 
 #update the Listbox
 def update(data):
@@ -41,19 +42,34 @@ my_label= Label(root, text="Start typing...",
 my_label.pack(pady=20)
 
 #create an entry box
-my_entry = Entry(root, font=("Helvetica",12))
+my_entry = Entry(root, font=("Helvetica",15), width=50)
 my_entry.pack()
 
 # Create a listbox
-my_list = Listbox(root, width=50)
+my_list = Listbox(root, width=100, height=200)
 my_list.pack(pady=40)
 
 #Create a list
 products=[]
+"""
 with open("database.txt") as database:
     lines=database.readlines()
     for line in lines:
         products.append(line)
+"""
+
+path="Publicaciones-2022_07_23-22_30.xls"
+excel_workbook = xlrd.open_workbook(path)
+excel_worksheet = excel_workbook.sheet_by_index(3)
+number_cols=excel_worksheet.ncols
+number_rows=excel_worksheet.nrows
+print("cols :"+ str(number_cols) + ", rows: " + str(number_rows))
+
+for row in range(1,number_rows):
+    for col in range(0,1):
+        #print(excel_worksheet.cell_value(row,col))
+        products.append(excel_worksheet.cell_value(row,col))
+
 
 update(products)
 
